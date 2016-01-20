3797914cf723975fe2f49c55a3b5d23f07bbf9a3
shafiq andisha <shafiq.andisha@gmail.com>
2016-01-20 13:17:46 +0900

    this program designed for analyzing DNS query. DNS query captured by tcpdump and then by using STDIN method sent the capture data to Ruby, in ruby I spilled the DNS query by spaces and then count the query if the query type is (A?)

--- tcpdump_analysis_01.rb
+++ tcpdump_analysis_01.rb
@@ -5,60 +5,66 @@ list_query = Hash.new
 
 # Trap break signal (CTRL+C) for output result
 Signal.trap(:INT) {
-    p count_query
-    p list_query
+  p count_query
+  p list_query
 }
 
 def output_result()
 end
 
 # Period in sec for result output
-period=300
+period=10
 last_timeslot = Time.now.to_i/period
 while (line=STDIN.gets) do
-    # The data contains tcpdump output as below.
-    # Ex:
-    # ["03:02:29.954497", "IP", "192.168.1.158.54397", ">", "210.130.1.1.53:", "11664+", "A?", "e6858.dscc.akamaiedge.net.0.1.cn.akamaiedge.net.", "(65)"]
-    # ["03:02:29.985184", "IP", "210.130.1.1.53", ">", "192.168.1.158.54397:", "11664", "1/0/0", "A", "104.110.7.129", "(81)"]
-    
-    period_timeslot = Time.now.to_i/period
-    data = line.strip.split(" ")
+  # The data contains tcpdump output as below.
+  # Ex:
+  # ["03:02:29.954497", "IP", "192.168.1.158.54397", ">", "210.130.1.1.53:", "11664+", "A?", "e6858.dscc.akamaiedge.net.0.1.cn.akamaiedge.net.", "(65)"]
+  # ["03:02:29.985184", "IP", "210.130.1.1.53", ">", "192.168.1.158.54397:", "11664", "1/0/0", "A", "104.110.7.129", "(81)"]
 
-    # Output splitted data for DEBUG
-    p data
+  period_timeslot = Time.now.to_i/period
+  data = line.strip.split(" ")
+  src = data[2].split(".")[0,4]
+ 
+  time = data[0].split(".")[0,1]
+  
+  # Pickup DNS query
+  if (data[6] == "A?") then
+  	 dst = data[4].split(".")[0,4]
+    # Initialize, variable preparation for 1st query
+    if (count_query[src] == nil) then
+      count_query[src] = 0
+    end
+    count_query[src] += 1
 
-    #len = data.length
-    #next if ( len == 0 )
-    #end
-    if (data.length != 0 )then
-    src = data[2].split(".")[0,4]
-    dst = data[3].split(".")[0,4]
-    
-    # Pickup DNS query
-    if (data[6] == "A?") then
-        # Initialize, variable preparation for 1st query
-        if (count_query[src] == nil) then
-            count_query[src] = 0
-        end
-        count_query[src] += 1
-        
-        # Initialize, Hash data preparation for 1st query
-        if (list_query[src] == nil) then
-            list_query[src] = Hash.new
-        end
-        # Initialize, variable data preparation for 1st query
-        if (list_query[src][data[7]] == nil) then
-            list_query[src][data[7]] = 0
-        end
-        list_query[src][data[7]] += 1
+    # Initialize, Hash data preparation for 1st query
+    if (list_query[src] == nil) then
+      list_query[src] = Hash.new
     end
+    # Initialize, variable data preparation for 1st query
+    if (list_query[src][data[7]] == nil) then
+    	#if (data[7] =~ /[0-9]+\/[0-9]+\//) then
+      	list_query[src][data[7]] = 0
+      	#end
     end
-    
-    # Check timeslot for controlling output timing
-    # Output analyzing result in each timeslot, specified by "period".
-    if (period_timeslot != last_timeslot) then
-        p count_query
-        p list_query
-    end
-    last_timeslot = period_timeslot
+    list_query[src][data[7]] += 1
+  end
+  #
+  if (data[6] =~ /[0-9]+\/[0-9]+\//) then
+  	mask = data[7]
+  	
+  #if (data[6] =~ /[0-9]/) then 
+#p line
+  end
+
+  # Check timeslot for controlling output timing
+  # Output analyzing result in each timeslot, specified by "period".
+  if (period_timeslot != last_timeslot) then
+    #p list_query
+    #p count_query
+  	print time, count_query, dst, mask
+  	p ""
+  end
+  last_timeslot = period_timeslot
+  	
 end
+
